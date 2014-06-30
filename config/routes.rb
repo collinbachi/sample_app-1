aeaee77e3c6b76f67fa8568219125da5ae1cf369
Kevin Button <kbutton16@gmail.com>
2014-06-30 15:09:02 -0400

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  #match '/index', to: 'static_pages#index', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
