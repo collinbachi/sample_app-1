3f7c59f11e2615a5543337787d68ca6540584b23
Kevin Button <kbutton16@gmail.com>
2014-06-30 12:29:21 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,13 +5,13 @@ describe "User pages" do
   subject { page }
  
 
- describe "profile page" do
-  let(:user) { FactoryGirl.create(:user) }
-  before { visit user_path(user) }
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-  it { should have_content(user.name) }
-  it { should have_title(user.name) }
-end
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
 
   describe "signup page" do
     before { visit signup_path }
@@ -20,7 +20,7 @@ end
     it { should have_title(full_title('Sign up')) }
   end
   
-describe "signup" do
+  describe "signup" do
 
     before { visit signup_path }
 
@@ -43,8 +43,20 @@ describe "signup" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link("Sign out") }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
- 
-
 end
